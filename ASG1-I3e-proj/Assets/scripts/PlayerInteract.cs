1315b8280704aea47586fe631156895343895559
KianMarcus <s10268765@connect.np.edu.sg>
2026-06-14 02:15:18 +0800

    Interactable gate

--- ASG1-I3e-proj/Assets/scripts/PlayerInteract.cs
+++ ASG1-I3e-proj/Assets/scripts/PlayerInteract.cs
@@ -3,21 +3,23 @@ using UnityEngine;
 public class PlayerInteract : MonoBehaviour
 {
     private Coin nearbyCoin;
+    private Door2 nearbyDoor;
  
     void OnTriggerEnter(Collider other)
     {
         nearbyCoin = other.GetComponent<Coin>();
+        nearbyDoor = other.GetComponent<Door2>();
     }
  
     void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<Coin>())
-            nearbyCoin = null;
+        if (other.GetComponent<Coin>()) nearbyCoin = null;
+        if (other.GetComponent<Door2>()) nearbyDoor = null;
     }
  
     void OnInteract()
     {
-        if (nearbyCoin != null)
-            nearbyCoin.Collect();
+        if (nearbyCoin != null) nearbyCoin.Collect();
+        if (nearbyDoor != null) nearbyDoor.Open();
     }
 }
